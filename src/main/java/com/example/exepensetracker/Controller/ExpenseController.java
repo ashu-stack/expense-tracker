fb3c631aa612f7fbd7bc879ef4fb0acb454ceaf6
Ashutosh Shukla <ashutoshhh37@gmail.com>
2025-12-18 16:04:23 +0530

    API-endpoints added in controllers

--- src/main/java/com/example/exepensetracker/Controller/ExpenseController.java
+++ src/main/java/com/example/exepensetracker/Controller/ExpenseController.java
@@ -1,15 +1,45 @@
 package com.example.exepensetracker.Controller;
 
+import com.example.exepensetracker.Entity.Expense;
 import com.example.exepensetracker.Service.ExpenseService;
 import jakarta.persistence.Entity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping (path="api/v1/expense")
 public class ExpenseController {
     private ExpenseService expenseService;
+
+    @GetMapping("/get-expenses")
+    public List<Expense> getAll(){
+        return null;
+    }
+
+    @GetMapping("/get-by-id/{myId}")
+    public Expense getById(@PathVariable Integer myID){
+        return new Expense();
+    }
+    //get by last week, month....????
+
+    @PostMapping("/add-expense")
+    public boolean addExpense(@RequestBody Expense expense){
+        // add function
+        return true;
+    }
+
+    @DeleteMapping("delete-by-id/{myId}")
+    public boolean deleteById(@PathVariable Integer myId){
+        //delete function
+        return true;
+    }
+
+    @PutMapping("update-by-id/{myId}")
+    public boolean updateById(@PathVariable Integer myId ,@RequestBody Expense expense ){
+        //update query by id and set using request body
+        return true;
+    }
 
 
 }
--- src/main/java/com/example/exepensetracker/Controller/UserController.java
+++ src/main/java/com/example/exepensetracker/Controller/UserController.java
@@ -1,11 +1,16 @@
 package com.example.exepensetracker.Controller;
 
 import com.example.exepensetracker.Service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/vi/user")
 public class UserController {
     private UserService userService;
+
+
 }
